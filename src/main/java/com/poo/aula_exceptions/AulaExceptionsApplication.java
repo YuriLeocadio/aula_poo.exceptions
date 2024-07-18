88ad67377aba7ef6efd916c89fc9bfe6442532a0
Yuri Moreira Leocadio <137840146+YuriLeocadio@users.noreply.github.com>
2024-07-18 00:34:37 +0000

    Criação do objeto exercicios para rodar o programa e criação do metodo limpa

--- src/main/java/com/poo/aula_exceptions/AulaExceptionsApplication.java
+++ src/main/java/com/poo/aula_exceptions/AulaExceptionsApplication.java
@@ -8,6 +8,25 @@ public class AulaExceptionsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AulaExceptionsApplication.class, args);
+		limpa();
+		
+		Exercicios exercicios = new Exercicios();
+		exercicios.ex1();
 	}
+
+	static void limpa() {
+        try {
+            if (System.getProperty("os.name").contains("Windows")) {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } else if (System.getProperty("os.name").contains("Linux")
+                    || System.getProperty("os.name").contains("Mac")) {
+                new ProcessBuilder("clear").inheritIO().start().waitFor();
+            } else {
+                System.out.println("Este comando não funciona no seu sistema operacional.");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 }
