376f0ca750f6ac4214bbba4c16078e3a35f9161b
Yuri Moreira Leocadio <yurileocadio0411@gmail.com>
2024-07-17 21:08:57 -0300

    first commit

---
+++ src/main/java/com/poo/aula_exceptions/AulaExceptionsApplication.java
@@ -0,0 +1,13 @@
+package com.poo.aula_exceptions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaExceptionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaExceptionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poo/aula_exceptions/AulaExceptionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poo.aula_exceptions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaExceptionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
