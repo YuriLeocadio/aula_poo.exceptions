7d2768b761aa1a667a1c84df8cb0d392c3529bad
Yuri Moreira Leocadio <137840146+YuriLeocadio@users.noreply.github.com>
2024-07-18 20:47:28 +0000

    Criação do Package exceptions para exceções personalizadas e criação da interface (MensagemError) para as mensagens de erro das exceções personalizadas

---
+++ src/main/java/com/poo/aula_exceptions/exceptions/MensagemError.java
@@ -0,0 +1,7 @@
+package com.poo.aula_exceptions.exceptions;
+
+public interface MensagemError {
+    String DIVISAO_POR_ZERO = "Divisão por zero não é permitida.";
+    String OPCOES_INVALIDAS = "Entrada inválida. Por favor, insira um número inteiro.";
+    String INDICE_INVALIDO = "Índice inválido. Escolha um índice entre 1 e 3.";
+}
